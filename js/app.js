741cc3a7b07e48234014b48ea8adb25989b2ae2e
eginugrahas <eginugrahasmart@gmail.com>
2021-11-13 18:24:24 +0700

    third commit

--- js/app.js
+++ js/app.js
@@ -2,8 +2,14 @@ const menu = document.querySelector('.menu');
 const btnHamburger = document.querySelector('.hamburger');
 const btnMenuClose = document.querySelector('#btnMenuClose');
 
+const cart = document.querySelector('.cart');
+const btnCart = document.querySelector('.btnCart');
+
+
 btnHamburger.addEventListener('click', onHamburgerClick);
 btnMenuClose.addEventListener('click', onBtnMenuCloseClick);
+
+btnCart.addEventListener('click', openCart);
 
 function onHamburgerClick() {
     menu.classList.remove('hidden');
@@ -11,4 +17,8 @@ function onHamburgerClick() {
 
 function onBtnMenuCloseClick() {
     menu.classList.add('hidden');
+}
+
+function openCart() {
+    cart.classList.toggle('hidden');
 }
